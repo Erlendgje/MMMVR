0000ae7fcde71ef7c7c7eae10119f6bc6c0fb3a8
NTNU04932\Dragvoll <simonstrand95@gmail.com>
2018-11-07 16:17:03 +0100

    Snap to position works

--- Assets/SnapToPosition.cs
+++ Assets/SnapToPosition.cs
@@ -4,67 +4,59 @@ using UnityEngine;
 
 public class SnapToPosition : MonoBehaviour {
 
-	private Rigidbody rigidbody;
-	private bool onTriggerStay = false;
+	private Rigidbody rgdb;
+    private bool isPickedUp = false;
+    private bool inTrgger = false;
 
 
 	// Use this for initialization
 	void Start () {
-		rigidbody = this.GetComponent<Rigidbody>();
+		rgdb = this.GetComponent<Rigidbody>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        //GameObject parent = this.gameObject.transform.parent.gameObject;
-        //print(gameObject.transform.position);
-   
-        //print("P1" + gameObject.transform.GetChild(0).transform.localPosition);
-        //print("P2" + gameObject.transform.GetChild(1).transform.localPosition);
-        //print("P3" + gameObject.transform.GetChild(2).transform.localPosition);
-
-        //Vector3 pos1 = gameObject.transform.GetChild(0).transform.localPosition;
-        //Vector3 pos2 = gameObject.transform.GetChild(1).transform.localPosition;
-        //Vector3 pos3 = gameObject.transform.GetChild(2).transform.localPosition;
-
-
-
-    }
 
     void OnTriggerStay(Collider other)
     {
-        print("P1" + other.transform.localPosition);
-        
-
-        float rotX = other.transform.localRotation.eulerAngles.x;
-        float rotY = other.transform.localRotation.eulerAngles.y;
-        float rotZ = other.transform.localRotation.eulerAngles.z;
-
-        float rotXstuff = rotX / 90;
-        float rotXstuffRound = Mathf.Round(rotXstuff);
-
-        float rotYstuff = rotY / 90;
-        float rotYstuffRound = Mathf.Round(rotYstuff);
-
-        float rotZstuff = rotZ / 90;
-        float rotZstuffRound = Mathf.Round(rotZstuff);
-
-        if (Mathf.Abs(other.transform.localPosition.x) <= 0.1 && Mathf.Abs(other.transform.localPosition.y) <= 0.1 && Mathf.Abs(other.transform.localPosition.z) <= 0.1)
+        if (isPickedUp)
         {
-            if (Mathf.Abs(rotXstuff - rotXstuffRound) * 90 < 5 || Mathf.Abs(rotYstuff - rotYstuffRound) * 90 < 5 || Mathf.Abs(rotZstuff - rotZstuffRound) * 90 < 5)
+            float rotX = transform.localRotation.eulerAngles.x;
+            float rotY = transform.localRotation.eulerAngles.y;
+            float rotZ = transform.localRotation.eulerAngles.z;
+
+            float rotXstuff = rotX / 90;
+            float rotXstuffRound = Mathf.Round(rotXstuff);
+
+            float rotYstuff = rotY / 90;
+            float rotYstuffRound = Mathf.Round(rotYstuff);
+
+            float rotZstuff = rotZ / 90;
+            float rotZstuffRound = Mathf.Round(rotZstuff);
+
+            if (Mathf.Abs(transform.localPosition.x) <= 0.1 && Mathf.Abs(transform.localPosition.y) <= 0.1 && Mathf.Abs(transform.localPosition.z) <= 0.1)
             {
-                other.transform.localPosition = new Vector3(0, 0, 0);
-                other.transform.localRotation = Quaternion.Euler(rotXstuffRound *90, rotYstuffRound *90, rotZstuffRound *90);
+                if (Mathf.Abs(rotXstuff - rotXstuffRound) * 90 < 5 || Mathf.Abs(rotYstuff - rotYstuffRound) * 90 < 5 || Mathf.Abs(rotZstuff - rotZstuffRound) * 90 < 5)
+                {
+                    transform.localPosition = new Vector3(0, 0, 0);
+                    transform.localRotation = Quaternion.Euler(rotXstuffRound * 90, rotYstuffRound * 90, rotZstuffRound * 90);
+                    inTrgger = true;
+                }
+                else { inTrgger = false; }
+            }
+            else
+            {
+                inTrgger = false;
             }
         }
     }
 
-	public void onPickUp() {
-		rigidbody.constraints = RigidbodyConstraints.None;
+    public void onPickUp() {
+		rgdb.constraints = RigidbodyConstraints.None;
+        isPickedUp = true;
 	}
 
 	public void onDetach() {
-		if(onTriggerStay) {
-			rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+        isPickedUp = false;
+		if(inTrgger) {
+			rgdb.constraints = RigidbodyConstraints.FreezeAll;
 		}
 	}
 }
