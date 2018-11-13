24dc5126e47d8ed8a5fe817e934f446ab0e5ad2c
unknown <erlendgje@gmail.com>
2018-11-13 15:07:15 +0100

    Can generate a prism

---
+++ Assets/GeneratePrism.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratePrism{
+
+
+
+	public List<Vector3[]> vertices;
+	public int[] indices = {
+		0, 2, 1,
+
+		3, 2, 0,
+		3, 5, 2,
+
+		1, 2, 5,
+		5, 4, 1,
+
+		1, 4, 0,
+		4, 3, 0,
+
+		3, 4, 5};
+
+	public GeneratePrism(int numberOfEdges, float radius, float height) {
+		vertices = new List<Vector3[]>();
+
+		Vector3[] points;
+
+		for(int i = 0; i < numberOfEdges; i++) {
+			points = new Vector3[6];
+			points[0] = new Vector3(0, 0, 0);
+			points[1] = Quaternion.AngleAxis(360/(float)numberOfEdges * i, Vector3.up) * new Vector3(radius, 0, 0);
+			points[2] = Quaternion.AngleAxis(360/(float)numberOfEdges * (i + 1), Vector3.up) * new Vector3(radius, 0, 0);
+			points[3] = new Vector3(0, height, 0);
+			points[4] = Quaternion.AngleAxis(360 / (float)numberOfEdges * i, Vector3.up) * new Vector3(radius, height, 0);
+			points[5] = Quaternion.AngleAxis(360 / (float)numberOfEdges * (i + 1), Vector3.up) * new Vector3(radius, height, 0);
+
+			vertices.Add(points);
+		}
+	}
+
+}
---
+++ Assets/Prism.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prism : MonoBehaviour {
+
+	GeneratePrism myPrism;
+	[SerializeField] GameObject prism;
+	// Use this for initialization
+	void Start () {
+		myPrism = new GeneratePrism(150, 1, 2);
+
+		foreach(Vector3[] v in myPrism.vertices) {
+			GameObject myGameObject = Instantiate(prism, this.transform);
+			Mesh mesh = new Mesh();
+			myGameObject.GetComponent<MeshFilter>().mesh = mesh;
+			mesh.vertices = v;
+			mesh.triangles = myPrism.indices;
+			MeshCollider mc = myGameObject.AddComponent<MeshCollider>();
+			mc.convex = true;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
