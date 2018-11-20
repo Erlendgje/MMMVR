2dd95ac6b73843e4ba0cfa8b5fed728308e033f7
unknown <erlendgje@gmail.com>
2018-11-20 09:34:00 +0100

    Mars and prism thing

--- Assets/Scripts/Prism.cs
+++ Assets/Scripts/Prism.cs
@@ -10,25 +10,48 @@ public class Prism : MonoBehaviour {
     [SerializeField] int number;
     [SerializeField] float radius;
     [SerializeField] float height;
+	[SerializeField] LinearMapping lm;
+	private float currentLM;
 	// Use this for initialization
 	void Start () {
+		createPrism(number, radius, height);
+		currentLM = lm.value;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(currentLM != System.Math.Round(lm.value, 2)) {
+			currentLM = (float)System.Math.Round(lm.value, 2);
+
+			foreach(Transform t in transform) {
+				Destroy(t.gameObject);
+			}
+
+			if(Mathf.Round(lm.value * 100) >= 3) {
+				createPrism((int)Mathf.Round(lm.value * 100), radius, height);
+			}
+			else {
+				createPrism(3, radius, height);
+			}
+		}
+
+	}
+
+	private void createPrism(int number, float radius, float height) {
 		myPrism = new GeneratePrism(number, radius, height);
 
 		foreach(Vector3[] v in myPrism.vertices) {
 			GameObject myGameObject = Instantiate(prism, this.transform);
+			myGameObject.transform.localPosition = new Vector3(0, 0, 0);
 			Mesh mesh = new Mesh();
 			myGameObject.GetComponent<MeshFilter>().mesh = mesh;
 			mesh.vertices = v;
 			mesh.triangles = myPrism.indices;
 			MeshCollider mc = myGameObject.AddComponent<MeshCollider>();
 			mc.convex = true;
-            myGameObject.AddComponent<VelocityEstimator>();
-            myGameObject.AddComponent<Throwable>();
+			//myGameObject.AddComponent<VelocityEstimator>();
+			//myGameObject.AddComponent<Throwable>();
 		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
 	}
 }
