208495365deeb6d04c44c004cf87d6b4eef8b9f7
Kakedragge <simonhst@stud.ntnu.no>
2018-12-03 11:37:14 +0100

    Grid stuff

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+
+	public Material emptyMaterial;
+
+	public Material filledMaterial;
+
+	public Renderer tileRenderer;
+
+	public void SetState(bool newState)
+	{
+		switch (newState)
+		{
+		case true:
+			if (tileRenderer != null && filledMaterial != null)
+			{
+				tileRenderer.sharedMaterial = filledMaterial;
+			}
+			break;
+		case false:
+			if (tileRenderer != null && emptyMaterial != null)
+			{
+				tileRenderer.sharedMaterial = emptyMaterial;
+			}
+			break;
+		}
+	}
+
+}
