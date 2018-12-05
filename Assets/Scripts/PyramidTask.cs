f7197dda90778ec8da18b4031cd29cb2025fc1de
Dragvoll <Dragvoll@NTNU07250.win.ntnu.no>
2018-12-05 17:29:05 +0100

    Success ish

--- Assets/Scripts/PyramidTask.cs
+++ Assets/Scripts/PyramidTask.cs
@@ -20,11 +20,18 @@ public class PyramidTask : Tasks {
 				return true;
 
 			})),
-            new Task("Bruk de fargede spakene til å lage en kube med volum på 100dm^3", "Volume", new System.Func<bool>(() => {
-                if(activeTaskObject.transform.localScale.x * activeTaskObject.transform.localScale.y * activeTaskObject.transform.localScale.z == 0.1) {
-                    return true;
+            new Task ("Vi har sett at volumet av en kube er l * b * h. Men hva er volumet til en pyramide? Fyll pyramider inn i kuben til den er full.", none, new System.Func<bool> (() => {
+
+
+
+                for (int i = 0; i < activeTaskObject.transform.childCount; i++) {
+                    Debug.Log("Hey" + i, activeTaskObject);
+                    if (!activeTaskObject.transform.GetChild(i).GetComponent<SnapToPosition>().getInTrigger()) {
+                        return false;
+                    }
                 }
-                return false;
+                return true;
+
             }))
         };
 		spawnObject();
