7ad3f30b2c711e6ea9b382e3f0eace8f6086e2ad
Kakedragge <simonhst@stud.ntnu.no>
2019-01-21 21:47:00 +0100

    Potato Farm initialized

---
+++ Assets/Scripts/ShapesTask.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapesTask : Tasks {
+
+	// Use this for initialization
+	void Start() {
+		tasks = new List<Task> {
+			new Task ("Potetene trenger vann. Ta formen som inneholder 10 liter vann og putt inn i vanningsskapet", "PotatoFarm", new System.Func<bool> (() => {
+
+				if (activeTaskObject.transform.GetChild(0).GetComponent<WateringLogic>().isCorrectContainer()) {
+					return true;	
+				}
+				return false;
+
+			}))
+		};
+		spawnObject();
+	}
+}
+
---
+++ Assets/Scripts/WateringLogic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class WateringLogic : MonoBehaviour
+{
+
+	public UnityEvent onContainerInBox;
+
+	protected virtual void OnTriggerStay(Collider other)
+	{
+			Debug.Log("Enter");
+			onContainerInBox.Invoke ();
+	}
+
+	public bool isCorrectContainer() {
+		return true;
+	}
+
+}
