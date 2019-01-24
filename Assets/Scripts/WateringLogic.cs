34f02b5bbf2d74d0af5c8b4efec6ba8fca67c27b
unknown <Dragvoll@NTNU00680.win.ntnu.no>
2019-01-24 16:17:40 +0100

    Animation on wateringDevice plusslogic

--- Assets/Scripts/WateringLogic.cs
+++ Assets/Scripts/WateringLogic.cs
@@ -9,45 +9,31 @@ public class WateringLogic : MonoBehaviour
 
 	public UnityEvent onContainerInBox;
     [SerializeField] public int wantedValue;
+    [SerializeField] public GameObject redLight = null;
 
+    private Animator _animator;
+    
 
-	private List<Vector3> tankPositions;
+	private List<Vector3> tankPositions = new List<Vector3>();
     private int totalValue = 0;
 
 
 	void Start()
 	{
-		for (var i = 0; i < transform.childCount; i++) {
-			tankPositions.Add (transform.GetChild (i).localPosition);
+
+        _animator = transform.Find("WateringDeviceDoor").GetComponent<Animator>();
+        for (var i = 0; i < transform.childCount; i++) {
+            
+            tankPositions.Add(transform.GetChild(i).localPosition);
 		}
 	}
 
 
 	protected virtual void OnTriggerEnter(Collider other)
 	{
-		Debug.Log(other.name);
+        //Debug.Log(other.name);
 
-        int otherValue = 0;
-
-        if (Int32.TryParse(other.name, out otherValue))
-        {
-            totalValue += otherValue;
-            if (totalValue == wantedValue)
-            {
-                onContainerInBox.Invoke();
-            }
-
-            other.gameObject.SetActive(false);
-
-            if (totalValue > wantedValue)
-            {
-                totalValue = 0;
-				respawnShapes ();
-            }
-
-            Debug.Log(totalValue.ToString());
-        }
-
+        StartCoroutine(closeDoor(other));
 
     }
 
@@ -57,10 +43,68 @@ public class WateringLogic : MonoBehaviour
 
     private void respawnShapes()
     {
-		for (var i = 0; i < transform.childCount; i++) {
-			transform.GetChild (i).localPosition = tankPositions[i];
-			transform.GetChild (i).gameObject.SetActive (true);
-		}
+        var childCount = transform.childCount;
+
+
+        for (var i = 0; i < childCount; i++) {
+
+            //Debug.Log(tankPositions[i].ToString() + "hey");
+
+            transform.GetChild(i).localPosition = tankPositions[i];
+            transform.GetChild(i).localRotation = new Quaternion(0, 0, 0, 0);
+            transform.GetChild (i).gameObject.SetActive (true);
+            
+        }
     }
+
+    IEnumerator closeDoor(Collider other)
+    {
+        int otherValue = 0;
+
+        if (Int32.TryParse(other.name, out otherValue))
+        {
+            
+            _animator.SetBool("open", true);
+            
+
+            yield return new WaitForSeconds(0.6f);
+            
+
+            totalValue += otherValue;
+            if (totalValue == wantedValue)
+            {
+                onContainerInBox.Invoke();
+            }
+
+            other.gameObject.SetActive(false);
+
+            yield return new WaitForSeconds(0.4f);
+
+            _animator.SetBool("open", false);
+
+
+            
+
+            if (totalValue > wantedValue)
+            {
+                totalValue = 0;
+                redLight.SetActive(true);
+                yield return new WaitForSeconds(0.1f);
+                redLight.SetActive(false);
+                yield return new WaitForSeconds(0.1f);
+                redLight.SetActive(true);
+                yield return new WaitForSeconds(0.1f);
+                redLight.SetActive(false);
+                yield return new WaitForSeconds(0.1f);
+                redLight.SetActive(true);
+                yield return new WaitForSeconds(0.1f);
+                respawnShapes();
+                redLight.SetActive(false);
+            }
+            
+
+        }
+    }
+
 
 }
