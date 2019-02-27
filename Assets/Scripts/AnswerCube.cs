1abd9a0677f0bfb1614768ec30405b9de03d440d
Magnus <magnus.warvik@gmail.com>
2019-02-27 17:56:56 +0100

    Answertable and stuff

---
+++ Assets/Scripts/AnswerCube.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnswerCube : MonoBehaviour
+{
+
+	public int answerInDm;
+	public int numbersInGroup;
+	public bool plane;
+
+}
