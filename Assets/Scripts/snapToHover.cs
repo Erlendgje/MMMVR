a8159935c7cafa4b800ffd84f0840e3076798204
marianylund <Dragvoll@NTNU07250.win.ntnu.no>
2019-03-05 15:44:57 +0100

    Working  pyramids on platforms

--- Assets/Scripts/snapToHover.cs
+++ Assets/Scripts/snapToHover.cs
@@ -7,29 +7,37 @@ public class snapToHover : MonoBehaviour
 
 	private bool inHand = false;
 
-	private Vector3 startPosition;
+	public float speed = 1;
 	private Rigidbody rgdb;
 
 	void Start() {
-		startPosition = transform.position;
 		rgdb = this.GetComponent<Rigidbody>();
 	}
 
 	void OnTriggerStay(Collider other) {
-
+        Debug.Log("before");
 		if (other.transform.tag == "Pillar" && !inHand) {
+            Debug.Log("after");
 
-			rgdb.constraints = RigidbodyConstraints.FreezeRotation;
+            rgdb.constraints = RigidbodyConstraints.FreezeRotation;
 			rgdb.constraints = RigidbodyConstraints.FreezePosition;
-			transform.position = startPosition;
-			transform.Rotate (0, Time.deltaTime*8, 0, Space.World);
+   
+            transform.position = new Vector3(other.transform.position.x, 1.7f, other.transform.position.z);
+            
+            transform.Rotate (0, Time.deltaTime*8, 0, Space.World);
 		}
 
 	}
 
-	void onPickUp() {
-		inHand = true;
+	public void OnPickUp() {
+        Debug.Log("pickup");
+        inHand = true;
 		rgdb.constraints = RigidbodyConstraints.None;
 	}
+
+    public void OnDetach()
+    {
+        inHand = false;
+    }
 
 }
