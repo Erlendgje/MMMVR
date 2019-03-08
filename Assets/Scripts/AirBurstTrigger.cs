1feabd8dc64022425443185e23f3e172977a7a12
Plohne <plohne95@gmail.com>
2019-03-08 14:48:36 +0100

    steamBurst and started on triggering trough animation

---
+++ Assets/Scripts/AirBurstTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirBurstTrigger : MonoBehaviour
+{
+
+    public void PlayParticle(GameObject par)
+    {
+        //activate particlesystem
+    }
+
+}
