ae4dffe19d7d72d78872ed226aa1fc9fead433b5
Kakedragge <simonhst@stud.ntnu.no>
2019-03-14 10:45:19 +0100

    Spray and pray

--- Assets/Scripts/AirBurstTrigger.cs
+++ Assets/Scripts/AirBurstTrigger.cs
@@ -5,15 +5,13 @@ using UnityEngine;
 public class AirBurstTrigger : MonoBehaviour
 {
 
-	[SerializeField] public GameObject particle1;
 	[SerializeField] public List<GameObject> particles;
-
+ 
     public void PlayParticle(int index)
     {
 		//activate particlesystem
-		Debug.Log("Particle System" + index);
-		//particle1.GetComponent<ParticleSystem>().Play();
 		particles[index].GetComponent<ParticleSystem>().Play();
+		transform.GetComponent<AudioSource>().Play ();
     }
 
 }
