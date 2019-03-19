11449a17e807965dff35af770b5add4110b82f77
unknown <erlendgje@gmail.com>
2019-03-19 16:14:41 +0100

    Added indicatior to pyramid build task

---
+++ Assets/Scripts/MaterialBlinking.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialBlinking : MonoBehaviour
+{
+
+	private Color c;
+
+	[SerializeField] private float from, to, speed;
+
+
+	private void Start() {
+		c = new Color();
+		c = this.GetComponent<MeshRenderer>().material.color;
+	}
+
+	public void startBlinking() {
+		StartCoroutine(blink());
+	}
+
+	private IEnumerator blink() {
+		while(true) {
+			while(c.a < to) {
+				Debug.Log(c.a + " : " + c[3]);
+				c[3] = c.a + speed * Time.deltaTime;
+				this.GetComponent<MeshRenderer>().material.color = c;
+				yield return null;
+			}
+			while(c.a > from) {
+				c[3] = c.a - speed * Time.deltaTime;
+				this.GetComponent<MeshRenderer>().material.color = c;
+				yield return null;
+			}
+		}
+	}
+}
