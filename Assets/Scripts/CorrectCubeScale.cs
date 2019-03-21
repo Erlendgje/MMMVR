7d27aa2f39332e4ddc5f59ea0db4c4c842e882df
unknown <erlendgje@gmail.com>
2019-03-21 16:03:58 +0100

    Changed texture for cube task

---
+++ Assets/Scripts/CorrectCubeScale.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorrectCubeScale : MonoBehaviour
+{
+	public void onCorrect() {
+		Array.Find(this.GetComponent<MeshRenderer>().materials, m => m.name.Equals("CableLight (Instance)")).EnableKeyword("_EMISSION");
+	}
+}
---
+++ Assets/Scripts/CorrectCubes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorrectCubes : MonoBehaviour
+{
+	[SerializeField] private MeshRenderer cable;
+	private Animator anim;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		anim = GetComponent<Animator>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+	public void onCorrect() {
+		anim.enabled = true;
+		Array.Find(cable.materials, m => m.name.Equals("CableLight (Instance)")).EnableKeyword("_EMISSION");
+		Array.Find(this.GetComponent<MeshRenderer>().materials, m => m.name.Equals("CableLight (Instance)")).EnableKeyword("_EMISSION");
+	}
+}
