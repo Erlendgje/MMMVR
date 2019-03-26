8e9a01532a2f86f4b0bbec5600afc734ccdbb0f7
unknown <erlendgje@gmail.com>
2019-03-26 14:36:11 +0100

    This might work?

---
+++ Assets/Scripts/Tablet.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Tablet : MonoBehaviour
+{
+	[SerializeField] private float speed;
+	[SerializeField] private RectTransform position;
+	private Vector3 fromPosition;
+
+	private void OnTriggerEnter(Collider other) {
+		if(other.gameObject.CompareTag("GameController")) {
+			fromPosition = other.transform.position;
+		}
+	}
+
+	private void OnTriggerStay(Collider other) {
+		if(other.gameObject.CompareTag("GameController")) {
+			Vector3 scrollValue = Vector3.Cross(this.transform.position, other.transform.rotation.eulerAngles / 360) - Vector3.Cross(fromPosition, other.transform.rotation.eulerAngles / 360);
+			position.position = new Vector3(scrollValue.x, position.position.y, position.position.z);
+		}
+	}
+}
