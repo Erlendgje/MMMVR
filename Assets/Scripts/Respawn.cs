c5a00788524472d0d10312b54af882c63f55f084
dragvoll <dragvoll@NTNU07075.win.ntnu.no>
2019-04-01 12:50:16 +0200

    Changed range of respawn cube

--- Assets/Scripts/Respawn.cs
+++ Assets/Scripts/Respawn.cs
@@ -6,18 +6,23 @@ public class Respawn : MonoBehaviour
 {
 
 	private Vector3 startPosition;
+	private Quaternion startRotation;
 
     // Start is called before the first frame update
     void Start()
     {
 		startPosition = this.transform.position;
+		startRotation = this.transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Vector3.Distance(startPosition, this.transform.position) > 20) {
+        if(Vector3.Distance(startPosition, this.transform.position) > 7) {
 			this.transform.position = startPosition;
+			this.transform.rotation = startRotation;
+			GetComponent<Rigidbody>().velocity = Vector3.zero;
+			GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
 		}
     }
 }
