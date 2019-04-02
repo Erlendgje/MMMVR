a7ca1d36b7a72c48c25a5d5b28ee6babce3521fb
Kakedragge <simonhst@stud.ntnu.no>
2019-04-02 14:26:12 +0200

    Battery test

---
+++ Assets/Battery.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battery : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+	void OnTriggerEnter(Collider other) {
+
+		if (other.transform.name == "sciFiTablet_redWhite") {
+			other.transform.GetComponent<OnTabletPickUp> ().enabled = true;
+			Destroy (gameObject);
+		}
+
+	}
+}
