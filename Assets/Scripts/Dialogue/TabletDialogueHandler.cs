0d534afdbea11affa88c41f8cf9f6a44fd735cb4
unknown <erlendgje@gmail.com>
2019-04-03 16:31:51 +0200

    Added story text file and can parse the json file

---
+++ Assets/Scripts/Dialogue/TabletDialogueHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class TabletDialogueHandler : MonoBehaviour
+{
+
+	private String dialogueFileName = "story.json";
+	private Dialogue dialogue;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		string filePath = Path.Combine(Application.streamingAssetsPath, dialogueFileName);
+
+		if(File.Exists(filePath)) {
+			string dataAsJason = File.ReadAllText(filePath);
+			dialogue = JsonUtility.FromJson<Dialogue>(dataAsJason);
+		}
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
